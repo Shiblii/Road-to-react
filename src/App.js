55d9904a45c289fa7ece568005482cc27b71bbe4
shiblii <hillaryiteba@gmail.com>
2019-08-19 12:35:40 +0300

    paginated fetch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const DEFAULT_QUERY = 'redux';
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE="page=";
 
 const largeColumn = {
   width: '40%',
@@ -19,8 +20,6 @@ const smallColumn = {
   width: '10%',
 };
 
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
   constructor(props) {
@@ -34,24 +33,47 @@ class App extends Component {
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+		this.onSearchSubmit =this.onSearchSubmit.bind(this);
+		this.fetchSearchTopStories =this.fetchSearchTopStories.bind(this);
   }
 
   setSearchTopStories(result) {
-    this.setState({ result });
+		const { hits, page } = result;
+const oldHits = page !== 0
+? this.state.result.hits : [];
+const updatedHits = [
+   ...oldHits,
+   ...hits
+   ];
+this.setState({
+result: { hits: updatedHits, page }
+});
   }
+
+	fetchSearchTopStories(searchTerm, page = 0){
+		fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
+			.then(response => response.json())
+			.then(result => this.setSearchTopStories(result))
+			.catch(error => error);
+
+
+	}
 
   componentDidMount() {
     const { searchTerm } = this.state;
 
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+this.fetchSearchTopStories(searchTerm);
   }
 
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
+
+	onSearchSubmit(event){
+const {searchTerm} =this.state;
+this.fetchSearchTopStories(searchTerm)
+event.preventDefault();
+	}
 
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
@@ -59,10 +81,11 @@ class App extends Component {
     this.setState({ result: {hits:updatedHits} });
   }
 
+
   render() {
     const { searchTerm, result } = this.state;
+const  page = (result && result.page) || 0
 
-    if (!result) { return null; }
 
     return (
       <div className="page">
@@ -70,6 +93,7 @@ class App extends Component {
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
+							onSubmit ={this.onSearchSubmit}
           >
             Search
           </Search>
@@ -80,23 +104,28 @@ class App extends Component {
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />:null}
+				<button onClick={() =>this.fetchSearchTopStories(searchTerm,page +1)}>Click me</button>
       </div>
     );
   }
 }
 
-const Search = ({ value, onChange, children }) =>
-  <form>
-    {children} <input
+const Search = ({ value, onChange, children ,onSubmit}) =>
+  <form onSubmit={onSubmit}>
+   <input
       type="text"
       value={value}
       onChange={onChange}
+
     />
+		<button type="submit">
+		{children}
+		</button>
   </form>
 
-const Table = ({ list, pattern, onDismiss }) =>
+const Table = ({ list,onDismiss }) =>
   <div className="table">
-    {list.filter(isSearched(pattern)).map(item =>
+    {list.map(item =>
       <div key={item.objectID} className="table-row">
         <span style={largeColumn}>
           <a href={item.url}>{item.title}</a>
