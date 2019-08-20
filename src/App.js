81938b3e3abeb9a43a9cb71887bf9a372e15c47d
shiblii <hillaryiteba@gmail.com>
2019-08-20 09:34:01 +0300

    table class component

--- src/App.js
+++ src/App.js
@@ -111,7 +111,7 @@ if(error){
     return (
       <div className="page">
         <div className="interactions">
-        
+
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
@@ -156,33 +156,41 @@ componentDidMount(){
 
 }
 
-const Table = ({ list,onDismiss }) =>
-  <div className="table">
-    {list.map(item =>
-      <div key={item.objectID} className="table-row">
-        <span style={largeColumn}>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span style={midColumn}>
-          {item.author}
-        </span>
-        <span style={smallColumn}>
-          {item.num_comments}
-        </span>
-        <span style={smallColumn}>
-          {item.points}
-        </span>
-        <span style={smallColumn}>
-          <Button
-            onClick={() => onDismiss(item.objectID)}
-            className="button-inline"
-          >
-            Dismiss
-          </Button>
-        </span>
-      </div>
-    )}
-  </div>
+class Table extends Component {
+  render(){
+  const   { list,onDismiss } =this.props
+    return(
+        <div className="table">
+          {list.map(item =>
+            <div key={item.objectID} className="table-row">
+              <span style={largeColumn}>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span style={midColumn}>
+                {item.author}
+              </span>
+              <span style={smallColumn}>
+                {item.num_comments}
+              </span>
+              <span style={smallColumn}>
+                {item.points}
+              </span>
+              <span style={smallColumn}>
+                <Button
+                  onClick={() => onDismiss(item.objectID)}
+                  className="button-inline"
+                >
+                  Dismiss
+                </Button>
+              </span>
+            </div>
+          )}
+        </div>
+
+    )
+  }
+}
+
 
 
 const Button = ({
